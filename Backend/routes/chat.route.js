bbb00465e47a497086427f9a575544ebe403ffde
halozra <revvv03@gmail.com>
2025-04-15 02:13:04 +0700

    ✨ Improve friend feature – update frontend UI & fix backend addFriend logic

--- Backend/routes/chat.route.js
+++ Backend/routes/chat.route.js
@@ -1,11 +1,18 @@
 import express from "express";
 import verifyToken from "../middleware/jwt.js";
-import { createChat, getUserChats } from "../controller/chat.controller.js";
+import {
+  addFriend,
+  createChat,
+  getFriend,
+  getUserChats,
+} from "../controller/chat.controller.js";
 
 const router = express.Router();
 
 router.post("/", verifyToken, createChat);
+router.post("/addFriend",verifyToken,addFriend)
 
 router.get("/getChatAllRoom", verifyToken, getUserChats);
+router.get("/getFriend", verifyToken, getFriend);
 
 export default router;
